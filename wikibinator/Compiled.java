6f87037c622fd04f3fd5c1b86898bd0053209f62
benrayfield <ben@humanai.net>
2020-12-31 13:03:33 -0500

    Compiled.java how to turing complete challenge response

--- wikibinator/Compiled.java
+++ wikibinator/Compiled.java
@@ -47,6 +47,35 @@ public interface Compiled extends BinaryOperator<fn>{
 		return on() ? this : prev().get();
 	}
 	
+	/** lazycl Blob, probably a LazyBlob. For number crunching such as 1 teraflops,
+	display of 3d fractals in realtime, matmul, music tools, etc.
+	When I say teraflop, I mean formal-verified IEEE754 float32 or float64 bits computed as lambdas
+	for even the smallest things like NAND of 2 bits, in interpreted mode (or various levels between using Compiled.setOn(boolean),
+	but in practice (TODO) will have a Compiled that calls lazycl in strict opencl mode to compute those exact float32 multiply bits etc,
+	which will generate the exact same merkle ids as the interpreted mode.
+	Or... If you do it thru Wiki.wiki which is a UnaryOperator<fn> and is 1 of the 8 opcodes to call wiki on 1 param,
+	then it may be nondeterministic without that formal-verification but is recommended to verify it
+	recursively and statistically using (L x (R x)) equals x forall x
+	and by turing-complete-challenge-response using various ids which are known to eval to the same thing
+	but those which have incomplete info might not know they have to return the same thing
+	so they can be caught in spreading unverified rumors or deception as theres an infinite number of ways
+	to write the same function if some parts are evaled and other parts are not evaled yet.
+	<br><br>
+	null if dont have this or if this Compiled is not a specific bitstring cuz its shared by many bitstrings,
+	in which case you should probably create a Blob view of that bitstring (or any powOf2 aligned subrange of it) thats relevant,
+	and create a Compiled whose prev() is this Compiled,
+	or something like that... I'm planning for linkedlists (anything made of pairs where u is R().R().R()...R()
+	to have a second kind of Compiled which is used by Curry op when Curry gets the second last thing in the linkedlist
+	to use as funcBody then calls funcBody on that linkedlist. The linkedlist should know that funcBody's Compiled
+	and with each nextParam prepended (appended as first thing in it), that pair should copy the ptr to that Compiled,
+	then Curry gets the Compiled without recursing into the linkedlist, so doesnt even have to go get the funcBody
+	since already have its Compiled if that Compiled somehow has a ptr to that funcBody but I'm not sure if thats a good idea
+	so might separately cache the second last thing in each linkedlist in each node in the linkedlist
+	(which never prevents garbcol since every fn that caches the second last thing in the linkedlist
+	can already reach that second last thing).
+	*/
+	public Blob cbt();
+	
 	//TODO
 
 }
