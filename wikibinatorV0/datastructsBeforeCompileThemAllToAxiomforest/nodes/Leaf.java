b2b585fc85c4e9a516bf1a50b3e11a16eae6b8fc
benrayfield <ben@humanai.net>
2021-01-15 15:18:47 -0500

    fgthdfsdf

--- wikibinatorV0/datastructsBeforeCompileThemAllToAxiomforest/nodes/Leaf.java
+++ wikibinatorV0/datastructsBeforeCompileThemAllToAxiomforest/nodes/Leaf.java
@@ -2,9 +2,6 @@
 package wikibinatorV0.datastructsBeforeCompileThemAllToAxiomforest.nodes;
 import wikibinatorV0.datastructsBeforeCompileThemAllToAxiomforest.Node;
 
-/** This is for efficient bitstrings of any size.
-Cbt aka complete binary tree of bits (pair of pair... of T or F).
-*/
 public class Leaf extends AbstractNode{
 	
 	public static final Leaf instance = new Leaf();
